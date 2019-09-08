072993746e0eff68b3d295609b0c98b4ff9a9745
Anton Bohomaz <bogomaz97@gmail.com>
2019-09-09 00:39:58 +0300

    airports client implemented

---
+++ src/components/AirportsPage.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "AirportsPage"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ tests/unit/mock-response-data.js
@@ -0,0 +1,55 @@
+export const testResponseData = {
+  "data": [
+    {
+      "id": 1,
+      "name": "Goroka Airport",
+      "city": "Goroka",
+      "country": "Papua New Guinea",
+      "iata": "GKA",
+      "icao": "AYGA",
+      "x": 145.391998291,
+      "y": -6.081689834590001,
+      "elevation": "5282",
+      "timezone": "10",
+      "dst": "U",
+      "tzid": "Pacific/Port_Moresby",
+      "type": "airport",
+      "source": "OurAirports"
+    },
+    {
+      "id": 2,
+      "name": "Madang Airport",
+      "city": "Madang",
+      "country": "Papua New Guinea",
+      "iata": "MAG",
+      "icao": "AYMD",
+      "x": 145.789001465,
+      "y": -5.20707988739,
+      "elevation": "20",
+      "timezone": "10",
+      "dst": "U",
+      "tzid": "Pacific/Port_Moresby",
+      "type": "airport",
+      "source": "OurAirports"
+    },
+    {
+      "id": 3,
+      "name": "Mount Hagen Kagamuga Airport",
+      "city": "Mount Hagen",
+      "country": "Papua New Guinea",
+      "iata": "HGU",
+      "icao": "AYMH",
+      "x": 144.29600524902344,
+      "y": -5.826789855957031,
+      "elevation": "5388",
+      "timezone": "10",
+      "dst": "U",
+      "tzid": "Pacific/Port_Moresby",
+      "type": "airport",
+      "source": "OurAirports"
+    }
+  ],
+  "totalNumberOfPages": 2566,
+  "page": 1,
+  "itemsPerPage": 3
+};
